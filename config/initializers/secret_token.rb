38055818ed9c3f3732330f704d6e935c6b5dc256
Ted Meeds <tmeeds@gmail.com>
2012-05-20 18:51:47 -0500

    created app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Caddev::Application.config.secret_token = '61fa1bb40855849f9fd9b87cc4553b7e8730e23f58cd70f59fdc91bb50496597c9ac6b786010f68584eaae361e01f20502608e4af3ad40ace7bfa3ce2bfbca9f'
