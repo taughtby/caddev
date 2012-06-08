ef646c56ea3be9d286f71535f450b6514b851bec
Carolina Gracia <carogracia@hotmail.com>
2012-06-08 18:32:40 -0500

    put zipcode loading into seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -52,6 +52,21 @@ puts "\t #{StudyGroupRegistration.count} registrations "
 puts "\t #{StudyGroupPost.count} posts"
 puts "\t #{StudyGroupComment.count} comments"
 
+puts "================ ZIP CODES ================"
+Zipcode.destroy_all
+
+fid = File.open( "doc/zipcodes.txt", "r")
+
+fid.each do |line|
+  # "17","60539","IL","MOOSEHEART",88.331532,41.824148,371,0.000032 
+  sp = line.split(",")
+  zipcode = sp[1][1..-2]
+  state   = sp[2][1..-2]
+  city    = sp[3][1..-2]
+
+  Zipcode.create( :code => zipcode, :city => city, :state => state )
+end
+fid.close()
 
 
 
